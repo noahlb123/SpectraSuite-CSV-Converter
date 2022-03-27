91a69abf3cad0203d480165d9b68d1cc319333ad
noahlb123 <noahliguoribills@gmail.com>
2022-03-26 21:58:43 -0400

    Added Batch Activity

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 import csv
+import os
 
-f = open("Ru.txt", "r")
-r = f.read()
-endIndex = r.index(">>>>>End Processed Spectral Data<<<<<") - 1
-startIndex = 1 + r.index(">>>>>Begin Processed Spectral Data<<<<<") + len(">>>>>Begin Processed Spectral Data<<<<<")
-values = r[startIndex:endIndex]
-specialChar = values[6]
-values = values.split("\n")
-for line in values:
-    line = line.split(specialChar)
+def TXTToCSV(name):
+    root = name.split("/")[-1]
+    root = root.split(".")[0]
+    return root + ".csv"
 
-with open("ru.csv", 'w') as newFile:
-    writer = csv.writer(newFile)
-    for i in values:
-        writer.writerow([i])
+for name in os.listdir("./spectrasuite-files"):
+    f = open("./spectrasuite-files/" + name, "r")
+    r = f.read()
+    endIndex = r.index(">>>>>End Processed Spectral Data<<<<<") - 1
+    startIndex = 1 + r.index(">>>>>Begin Processed Spectral Data<<<<<") + len(">>>>>Begin Processed Spectral Data<<<<<")
+    values = r[startIndex:endIndex]
+    specialChar = values[6]
+    values = values.split("\n")
+    for line in values:
+        line = line.split(specialChar)
 
-print("sucess")
+    with open("./output/" + TXTToCSV(name), 'w') as newFile:
+        writer = csv.writer(newFile)
+        for i in values:
+            writer.writerow([i])
+
+    print("processed", name)
